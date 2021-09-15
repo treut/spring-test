13c1e5a9bec7d282814f81f9b1227527a17904b0
treut <d@treut.ru>
2021-09-15 11:12:02 +0300

    change bean

--- springd/src/main/java/ru/treut/spring1/TestSpring.java
+++ springd/src/main/java/ru/treut/spring1/TestSpring.java
@@ -1,14 +1,24 @@
 package ru.treut.spring1;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
 
+
+//@Controller
 public class TestSpring {
     public static void main(String args[]){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
 
-        System.out.println(testBean.getName());
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        //while(true){
+            try{
+                Thread.sleep(100);
+                System.out.println(testBean.getName());
+            }
+            catch(Exception e){}
+        //}
         context.close();
     }
 }

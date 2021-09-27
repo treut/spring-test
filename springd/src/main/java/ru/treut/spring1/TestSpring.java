37c755c10eba43798d539b62f8246d396055a041
treut <d@treut.ru>
2021-09-27 21:24:38 +0300

    upd

--- springd/src/main/java/ru/treut/spring1/TestSpring.java
+++ springd/src/main/java/ru/treut/spring1/TestSpring.java
@@ -18,6 +18,9 @@ public class TestSpring {
         MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
         mp.playMusic();
 
+        System.out.println(mp.getName());
+        System.out.println(mp.getVolume());
+
 
         //while(true){
             /*try{

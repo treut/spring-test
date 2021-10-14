a242297f88dc6913e2e651ae7c39a18f84ceaa21
treut <d@treut.ru>
2021-10-14 03:32:13 +0300

    Spring DI

--- spr1/src/main/java/ru/treut/spr1/Test.java
+++ spr1/src/main/java/ru/treut/spr1/Test.java
@@ -3,13 +3,13 @@ package ru.treut.spr1;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-    public static void main(String args[]){
+    public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-        Music mb = context.getBean("mBean", Music.class);
+//        Music mb = context.getBean("mBean", Music.class);
+//        Player mp=new Player(mb);
 
-        Player mp=new Player(mb);
-
+        Player mp=context.getBean("player", Player.class);
         mp.playMusic();
 
         context.close();

05f38b0aa14b036c4bcdf04f2cdc33695de97ab9
treut <d@treut.ru>
2021-10-20 22:00:23 +0300

    init & destroy methods

--- spr1/src/main/java/ru/treut/spr1/RMusic.java
+++ spr1/src/main/java/ru/treut/spr1/RMusic.java
@@ -1,6 +1,15 @@
 package ru.treut.spr1;
 
 public class RMusic implements Music{
+
+    public void inita(){
+        System.out.println("Initialization");
+    }
+
+    public void destro(){
+        System.out.println("Destroyed");
+    }
+
     public String getSong(){
         return "Rock";
     }
--- spr1/src/main/java/ru/treut/spr1/Test.java
+++ spr1/src/main/java/ru/treut/spr1/Test.java
@@ -6,14 +6,18 @@ public class Test {
     public static void main(String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
+        RMusic rm=context.getBean("mBean", RMusic.class);
+
+        System.out.println(rm.getSong());
+
 //        Music mb = context.getBean("mBean", Music.class);
 //        Player mp=new Player(mb);
 
-        Player mp=context.getBean("player", Player.class);
-        mp.playMusic();
-
-        System.out.println(mp.getName());
-        System.out.println(mp.getVolume());
+//        Player mp=context.getBean("player", Player.class);
+//        mp.playMusic();
+//
+//        System.out.println(mp.getName());
+//        System.out.println(mp.getVolume());
 
         context.close();
     }

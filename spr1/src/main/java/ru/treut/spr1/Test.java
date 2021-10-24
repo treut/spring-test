00afab40b0391a4cc84670a47011cdff4c1c0eed
treut <d@treut.ru>
2021-10-24 19:16:31 +0300

    edit

--- spr1/src/main/java/ru/treut/spr1/Test.java
+++ spr1/src/main/java/ru/treut/spr1/Test.java
@@ -1,28 +1,11 @@
 package ru.treut.spr1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args){
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-
-//        AnnotationConfigApplicationContext
-
-//        Music rm=context.getBean("mBean", Music.class);
-
-//        System.out.println(rm.getSong());
-
-
-//        Music mb = context.getBean("mBean", Music.class);
-//        Player mp=new Player(mb);
-
-//        Player mp=context.getBean("player", Player.class);
-//        mp.playMusic();
-//
-//        System.out.println(mp.getName());
-//        System.out.println(mp.getVolume());
 
         Player pp=context.getBean("player", Player.class);
         pp.playMusic();

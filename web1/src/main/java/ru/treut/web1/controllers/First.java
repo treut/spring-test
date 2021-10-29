b4b9efad1905a6840dd1534425c46de33aabba6b
treut <d@treut.ru>
2021-10-29 16:45:34 +0300

    Model

--- web1/src/main/java/ru/treut/web1/controllers/First.java
+++ web1/src/main/java/ru/treut/web1/controllers/First.java
@@ -1,6 +1,7 @@
 package ru.treut.web1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 public class First {
 
     @GetMapping("/halo")
-    public String halo(@RequestParam(value="name", required=false) String name){
+    public String halo(@RequestParam(value="name", required=false) String name, Model model){
 //    public String halo(HttpServletRequest request){
 //        String name=request.getParameter("name");
 
-        System.out.println("NAME: "+name);
+        //        System.out.println("NAME: "+name);
+        model.addAttribute("name", name);
 
         return "first/halo";
     }

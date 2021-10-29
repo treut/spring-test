3185fe14581e41c14962dd366005a647c1b06ef9
treut <d@treut.ru>
2021-10-29 16:10:40 +0300

    request param

--- web1/src/main/java/ru/treut/web1/controllers/First.java
+++ web1/src/main/java/ru/treut/web1/controllers/First.java
@@ -3,13 +3,21 @@ package ru.treut.web1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/ff")
 public class First {
 
     @GetMapping("/halo")
-    public String halo(){
+    public String halo(@RequestParam(value="name", required=false) String name){
+//    public String halo(HttpServletRequest request){
+//        String name=request.getParameter("name");
+
+        System.out.println("NAME: "+name);
+
         return "first/halo";
     }
 

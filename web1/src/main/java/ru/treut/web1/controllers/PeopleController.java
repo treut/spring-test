34ec7d01b0598acf3ebcd4c26f7beeacfbc665d1
treut <d@treut.ru>
2021-10-31 22:54:37 +0300

    add new person

--- web1/src/main/java/ru/treut/web1/controllers/PeopleController.java
+++ web1/src/main/java/ru/treut/web1/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.treut.web1.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.treut.web1.dao.PersonDAO;
+import ru.treut.web1.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +27,18 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model){
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+//    public String newPerson(Model model){
+    public String newPerson(@ModelAttribute("person") Person person){
+//        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
